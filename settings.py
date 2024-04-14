8f295004b69a00f99836c6d0ec7761041927922b
egorghb <egor.ponomaryov2002@gmail.com>
2024-04-14 16:26:57 +0500

    update

--- settings.py
+++ settings.py
@@ -1,8 +1,11 @@
 import json
+import os
 
 
 def load_settings(filename="settings.json"):
     """Загружает настройки приложения"""
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    filename = os.path.join(current_dir, filename)
     try:
         with open(filename, "r") as f:
             return json.load(f)
@@ -12,8 +15,9 @@ def load_settings(filename="settings.json"):
 
 settings = load_settings()
 
-show_fps = settings.get("show_fps", True)
-draw_hand_skeleton = settings.get("draw_hand_skeleton", False)
-draw_bounding_box = settings.get("draw_bounding_box", True)
-draw_danger_zone = settings.get("draw_danger_zone", True)
-camera_path = settings.get("camera_path", 0)
+if settings:
+    show_fps = settings.get("show_fps", True)
+    draw_hand_skeleton = settings.get("draw_hand_skeleton", False)
+    draw_bounding_box = settings.get("draw_bounding_box", True)
+    draw_danger_zone = settings.get("draw_danger_zone", True)
+    camera_path = settings.get("camera_path", 0)
